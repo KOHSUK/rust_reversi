9106525f73df2ef4925e3dfe957bf747ff82aa3d
Kosuke Kihara <ashenden1965@gmail.com>
2020-05-31 09:59:39 +0900

    Boardの初期化処理を追加、現在のBoardの状態を出力可能に

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,54 @@ use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
+use std::fmt;
+
+const BOARD_SIZE: i32 = 64;
+
+struct Board {
+    white: u64,
+    black: u64,
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            white: 0x0000001008000000,
+            black: 0x0000000810000000,
+        }
+    }
+}
+
+// println!マクロで指定フォーマットで出力できるように
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut board = String::new();
+        for i in 0..BOARD_SIZE {
+            let bit_pos = i as u64;
+            if get_bit(&self.white, &bit_pos)==1 as u64 {
+                board.push_str("[@]");
+            } else if get_bit(&self.black, &bit_pos)==1 as u64 {
+                board.push_str("[O]");
+            } else {
+                board.push_str("[ ]");
+            }
+
+            if ((i+1) % 8) == 0 { 
+                board.push_str("\r\n");
+            }
+        }
+        write!(f, "{}", board)
+    }
+}
+
+fn get_bit(bitboard: &u64, pos: &u64) -> u64 {
+    return (bitboard >> pos) & (1 as u64);
+}
+
+fn print_test() {
+    let board = Board::new();
+    println!("{}", board);
+}
 
 fn main() {
     let stdin = stdin();
@@ -21,11 +69,13 @@ fn main() {
     // writeln使わない場合、stdoutはbufferingするらしいので、
     //　即座に反映させる
     stdout.flush().unwrap();
+
+    print_test();
     
     for c in stdin.events() {
         let evt = c.unwrap();
 
-        write!(stdout, "{}", "Test").unwrap();
+        println!("{}", "test");
 
         match evt {
             // Ctrl-cでアプリケーション終了
